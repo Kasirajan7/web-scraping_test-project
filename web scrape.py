fdc3be84e5a25f5fb6a7323e983450ca2e649cb2
Kasirajan <kasimurugasen1995@gmail.com>
2017-11-23 15:28:12 +0530

    I just scrap all the item details from the eCommerce and stored it in the csv file

---
+++ web scrape.py
@@ -0,0 +1,34 @@
+import io
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+#BeatifulSoup parse the html text
+#opening the connection and grapping the info
+my_url="https://www.flipkart.com/search?q=dslr&sid=jek/p31/trv&as=on&as-show=on&otracker=start&as-pos=1_1_ic_dslr"
+uClient=uReq(my_url)
+page_html=uClient.read()
+uClient.close()
+#html parser
+page_soup=soup(page_html,"html.parser")
+
+#grabs each product
+
+containers=page_soup.findAll("div",{"class":"_3liAhj"})
+filename="product.csv"
+f=io.open(filename,"w",encoding="utf-8")
+headers="ProductName , Rating , Price \n"
+f.write(headers)
+for container in containers:
+    product=container.div.div.div.img["alt"]
+    rating=container.findAll("div",{"class":"hGSR34 _2beYZw"})
+    price=container.findAll("div",{"class":"_1vC4OE"})
+    price_value=price[0].text
+    
+    try:
+        rating_value=rating[0].text
+    except IndexError:
+        rating_value="No ratings "
+    
+
+    f.write(product + "," + rating_value[:-1] + "," + price_value[1:].replace(",","") +"\n")
+
+f.close()
